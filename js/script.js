b011a41efa9f82eaaa9b2d81578e34aa787d2ee3
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-21 14:34:16 +0200

    find correct article using previously established selector and add class active to the correct article

--- js/script.js
+++ js/script.js
@@ -8,8 +8,6 @@
 const titleClickHandler = function(event){
     event.preventDefault();
     const clickedElement = this;
-    console.log('Link was clicked!')
-    console.log(event);
 
   /* [DONE] remove class 'active' from all article links  */
 
@@ -22,7 +20,6 @@ const titleClickHandler = function(event){
   /* [DONE] add class 'active' to the clicked link */
 
     clickedElement.classList.add('active');
-    console.log('clickedElement:', clickedElement);
 
   /* [DONE] remove class 'active' from all articles */
 
@@ -34,15 +31,15 @@ const titleClickHandler = function(event){
  
   /* [DONE] get 'href' attribute from the clicked link */
 
-    const attribute = clickedElement.getAttribute("href");
-    console.log(attribute);
+    const articleSelector = clickedElement.getAttribute("href");
 
-  /* find the correct article using the selector (value of 'href' attribute) */
+  /* [DONE] find the correct article using the selector (value of 'href' attribute) */
 
+    const targetArticle = document.querySelector(articleSelector);
 
+  /* [DONE] add class 'active' to the correct article */
 
-  /* add class 'active' to the correct article */
-
+    targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
