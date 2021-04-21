4e0b0f70623449cb513732696f50243ab263a700
Sylwester Zajac <sylwekkza@gmail.com>
2021-04-21 13:22:49 +0200

    add function to remove class active form all articles and article links

--- js/script.js
+++ js/script.js
@@ -1,7 +1,5 @@
 'use strict';
 
-document.write('Hello World');
-
 /* document.getElementById('test-button').addEventListener('click', function(){
     const links = document.querySelectorAll('.titles a');
     console.log('links:', links);
@@ -13,9 +11,21 @@ const titleClickHandler = function(event){
 }
   /* remove class 'active' from all article links  */
 
+const activeLinks = document.querySelectorAll('.titles a.active');
+  
+for(let activeLink of activeLinks){
+    activeLink.classList.remove('active');
+  }
+  
   /* add class 'active' to the clicked link */
 
   /* remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('.post.active');
+
+for(let activeArticle of activeArticles){
+     activeArticle.classList.remove('active');
+ }
 
   /* get 'href' attribute from the clicked link */
 
